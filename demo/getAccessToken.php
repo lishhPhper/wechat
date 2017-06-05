7fa3ba866d5462776ac2bc9064aab208b3ec6046
lishhPhper <nietzsche_nc@163.com>
2017-06-05 10:25:37 +0800

    微信自定义菜单项

--- demo/getAccessToken.php
+++ demo/getAccessToken.php
@@ -4,10 +4,8 @@ include '../vendor/autoload.php';
 $appid = "wx4798240534cc014b";
 $appsecret = "60619753f72d3dcdd704e48c5aa589db";
 
-/*$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;*/
+$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
 
-$url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
-$post_data = array ("appid" => "wx4798240534cc014b","secret" => "60619753f72d3dcdd704e48c5aa589db");
 
 
 // 1. cURL初始化
@@ -19,16 +17,9 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 */
-/*curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);*/
-
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-　　// post数据
-curl_setopt($ch, CURLOPT_POST, 1);
-　　// post的变量
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
 // 3. 执行cURL请求
 $ret = curl_exec($ch);
