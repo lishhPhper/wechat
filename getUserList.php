844cef48ac5e755305b1171342e3abed9ba27ef7
lishhPhper <nietzsche_nc@163.com>
2017-06-05 23:20:39 +0800

    微信获取openId

--- getUserList.php
+++ getUserList.php
@@ -8,8 +8,8 @@ $wechatObj = new wxModel();
 echo $wechatObj->getUserOpenIdList();
 
 $url = "https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token".$wechatObj->getAccessToken();
-
-echo $wechatObj->getMediaID('./public/zhuren.jpg');
+dump($wechatObj->getAccessToken());
+//dump($wechatObj->getMediaID('./public/zhuren.jpg'));
 
 
 
